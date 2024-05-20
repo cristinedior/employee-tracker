32dfef3abdbfafa2d0799a791814deefb4e509d4
cristinedior <148567509+cristinedior@users.noreply.github.com>
2024-05-20 16:00:23 -0500

    commiting new schema and seed code and added password to index.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,23 @@
-CREATE DATABASE employee_tracker;
-
+CREATE DATABASE IF NOT EXISTS employee_tracker;
 USE employee_tracker;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id),
